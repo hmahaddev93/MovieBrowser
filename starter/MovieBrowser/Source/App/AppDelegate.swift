1beb57ed435e4d18144d763a3c37feb8f76e10ed
Khatib <hmahaddev93@gmail.com>
2021-10-19 21:01:29 -0500

    navigation bar update for iOS15

--- starter/MovieBrowser/Source/App/AppDelegate.swift
+++ starter/MovieBrowser/Source/App/AppDelegate.swift
@@ -11,6 +11,16 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        if #available(iOS 15, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            appearance.backgroundColor = .systemBlue
+            UINavigationBar.appearance().standardAppearance = appearance
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        }
+        
         UINavigationBar.appearance().barStyle = .black
         UINavigationBar.appearance().barTintColor = .systemBlue
         UINavigationBar.appearance().tintColor = .white
